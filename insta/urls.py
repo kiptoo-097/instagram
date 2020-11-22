8ae3ee9c74b57ba0cafafb6cd7d1e8dbb4337285
kiptoo-097 <kiptoo097.com>
2020-11-23 00:04:08 +0300

    Added home url pattern in urls.py

--- insta/urls.py
+++ insta/urls.py
@@ -1,1 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+from . import views
+
+
+urlpatterns=[
+    path('',views.home,name='home'),
+   
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- instagram/settings.py
+++ instagram/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
 import os
 
 
