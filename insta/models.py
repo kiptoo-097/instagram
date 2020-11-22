a9371b8899f55edf770d46877649cf4c1607b676
kiptoo-097 <kiptoo097.com>
2020-11-23 00:28:14 +0300

    Added class picture

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+
+class Picture(models.Model):
+    image = models.ImageField(upload_to='images/')
+    picture_name = models.CharField(max_length=50)
+    picture_caption = models.CharField(max_length=50)
+    likes = models.PositiveIntegerField(default=0)
+    
+    
+    def save_picture(self):
+        self.save()
+        
+    def delete_picture(self):
+        self.delete()
+        
+    def __str__(self):
+        
+        return self.picture_name
