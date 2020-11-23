ea03aba0d913564852e7d4adc9d72f4d756efb70
kiptoo-097 <kiptoo097.com>
2020-11-24 02:08:02 +0300

    Imported cloudinary, settings.py file

--- instagram/settings.py
+++ instagram/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
