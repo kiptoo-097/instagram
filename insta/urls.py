be62a428c6efdab3d59d5e50d3e68e4a54837096
kiptoo-097 <kiptoo097.com>
2020-11-24 02:29:13 +0300

    Added url paths to respective views

--- insta/urls.py
+++ insta/urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
+from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include
 from . import views
+from django.conf.urls import url
+
+
 
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('image/', views.image_upload,name='upload'),
+    path('profile/', views.profile_info,name='profile'),
+    path('edit/',views.profile_edit,name='edit'),
+    url(r'^new_comment/(\d+)/$' ,views.add_comment,name='newComment'),
+    url(r'^comment/(\d+)/$' ,views.comments,name='comments'),
+    url(r'^likes/(\d+)/$' , views.like_images, name='likes'),
+    path('user/',views.search_user,name='search_user'),
    
 ]
 if settings.DEBUG:
