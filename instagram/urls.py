a2517270a91b0b9134bc9eb1944990af299f62be
kiptoo-097 <kiptoo097.com>
2020-11-24 02:15:15 +0300

    Imported views and auth views

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
+    path('about/',include('insta.urls')),
+    
+    
 ]
